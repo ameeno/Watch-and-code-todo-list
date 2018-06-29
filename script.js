515e90c475fa92c29105c045b99af9521cd7f8eb
Arun Shah <fastah@gmail.com>
2018-06-29 11:20:43 +0100

    I have added nested display functions

--- script.js
+++ script.js
@@ -3,18 +3,21 @@
  */
 var todos = [];
 
-function showTodoList() {
+function displayTodos() {
     console.log("my Todos: ", todos);
 }
 
 function addTodo(todo) {
     todos.push(todo);
+    displayTodos();
 }
 
 function changeTodo(num, todo) {
     todos[num] = todo;
+    displayTodos();
 }
 
 function deleteTodo(num) {
     todos.splice(num, 1);
+    displayTodos();
 }
